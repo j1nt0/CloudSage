1d2a2f910e9c7a58a9a263b37d10e1c72347d8b0
jin <ljin6381@naver.com>
2024-08-06 01:17:21 +0900

    Initial Commit

---
+++ CloudSage/CloudSageApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudSageApp.swift
+//  CloudSage
+//
+//  Created by Jin Lee on 8/6/24.
+//
+
+import SwiftUI
+
+@main
+struct CloudSageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
